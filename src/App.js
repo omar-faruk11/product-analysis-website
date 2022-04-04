7c0d9cc5530b29f042fee21ac87f4a15c5708009
omar-faruk11 <omarabdullah1811@gmail.com>
2022-04-04 06:09:40 +0600

    Simple react router added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
+import Blogs from './Components/Blogs/Blogs';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Error from './Components/Error/Error';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import Reviews from './Components/Reviews/Reviews';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/reviews' element={<Reviews/>}></Route>
+        <Route path='/dashboard' element={<Dashboard/>}></Route>
+        <Route path='/blogs' element={<Blogs/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='*' element={<Error/>}></Route>
+      </Routes>
+    </>
   );
 }
 
