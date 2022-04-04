624c873c31a32cbbd0d90b2b4a2a9bccc64aef9f
omar-faruk11 <omarabdullah1811@gmail.com>
2022-04-04 07:44:52 +0600

    Header added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
+    const textDecortion = { textDecoration: 'none',padding:'5px' }
     return (
-        <nav>
-            
+        <nav className=' d-flex justify-content-center align-items-center p-3 bg-light'>
+                <div className=' mx-4 fw-bold'>
+                    <NavLink className={({ isActive }) => isActive ? 'text-danger' : 'text-dark'} style={textDecortion} to='/'> Home</NavLink>
+                </div>
+                <div className=" mx-4 fw-bold">
+                    <NavLink className={({ isActive }) => isActive ? 'text-danger' : 'text-dark'} style={textDecortion} to='/reviews'>Reviews</NavLink>
+                </div>
+                <div className=" mx-4 fw-bold">
+                    <NavLink className={({ isActive }) => isActive ? 'text-danger' : 'text-dark'} style={textDecortion} to='/dashboard'> Dashboad</NavLink>
+                </div>
+                <div className=" mx-4 fw-bold">
+                    <NavLink className={({ isActive }) => isActive ? 'text-danger' : 'text-dark'} style={textDecortion} to='/blogs'> Blogs</NavLink>
+                </div>
+                <div className=" mx-4 fw-bold">
+                    <NavLink className={({ isActive }) => isActive ? 'text-danger' : 'text-dark'} style={textDecortion} to='/about'>About</NavLink>
+                </div>
         </nav>
     );
 };
